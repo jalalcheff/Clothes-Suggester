4a7cb80292e01d66620c750685d5c754ba7da5e6
jalal cheff <jlalalmyahy571@gmail.com>
2023-04-08 16:26:14 +0300

    create shared prefs object

---
+++ app/src/main/java/com/example/clothessuggester/util/UtilSharedPrefs.kt
@@ -0,0 +1,20 @@
+package com.example.clothessuggester.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object UtilSharedPrefs {
+    private var sharedPrefs:SharedPreferences?=null
+    private const val SHAREDPREFNAME="weather"
+    private const val WEATHER_COLD="weatherCold"
+    fun initPrefsUtil(context: Context)
+    {
+        sharedPrefs=context.getSharedPreferences(SHAREDPREFNAME,Context.MODE_PRIVATE)
+    }
+    var user:String?
+    get() = sharedPrefs?.getString(WEATHER_COLD,null)
+    set(value) {
+        sharedPrefs?.edit()?.putString(WEATHER_COLD,value)
+    }
+
+}
