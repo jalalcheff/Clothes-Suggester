1e7797e5cce04a3977229e37cd07849ff306e86e
jalal cheff <jlalalmyahy571@gmail.com>
2023-04-13 13:51:42 +0300

    convert presenter functions to higher order function

--- app/src/main/java/com/example/clothessuggester/presnter/MainPresenter.kt
+++ app/src/main/java/com/example/clothessuggester/presnter/MainPresenter.kt
@@ -1,38 +1,40 @@
 package com.example.clothessuggester.presnter
 
+import com.example.clothessuggester.model.domain.WeatherStatus
 import com.example.clothessuggester.model.domain.api.WeatherStatusInfo
 import com.example.clothessuggester.model.domain.local.ModifySharedPrefs
 import com.example.clothessuggester.ui.IMainView
+import okio.IOException
 
 class MainPresenter {
     lateinit var iMainView: IMainView
-    private var tempreature:Double?=null
+    private var temperature: Double? = null
     fun presentData() {
-       /* WeatherStatusInfo().getDegree { weatherStatus ->
-            itemperautre.onSuccessWeatherTemperature(weatherStatus.main.temp)
-        }*/
         WeatherStatusInfo().getWeatherStatusResponse(
-            {weatherStatus->
-                tempreature=weatherStatus.main.temp
-                iMainView.onSuccessWeatherTemperature(weatherStatus.main.temp)
-                iMainView.onSuccessCityName(weatherStatus.name)
-            }
-        ,
-            {e->
-                iMainView.onFailureWeatherTemperature(e)
-                iMainView.onFailureCityName(e)
-            }
+            ::onGetWeatherStatusTemperatureSuccess,
+            ::onGetWeatherStatusTemperatureFailure
         )
-
-        ModifySharedPrefs().getDataFromSharedPrefs { clothesImages ->
-            iMainView.onSuccessImageDrawable(clothesImages)
-        }
-
+        ModifySharedPrefs().getDataFromSharedPrefs(::onGetClothesFromSharedPreferences)
     }
-    fun rePresentData()
-    {
-        ModifySharedPrefs().saveData(tempreature)
-        ModifySharedPrefs().getDataFromSharedPrefs { clothesImages->
+
+    private fun onGetWeatherStatusTemperatureSuccess(weatherStatus: WeatherStatus) {
+        temperature = weatherStatus.main.temp
+        iMainView.onSuccessWeatherTemperature(weatherStatus.main.temp)
+        iMainView.onSuccessCityName(weatherStatus.name)
+    }
+
+    private fun onGetWeatherStatusTemperatureFailure(exception: IOException) {
+        iMainView.onFailureWeatherTemperature(exception)
+        iMainView.onFailureCityName(exception)
+    }
+
+    private fun onGetClothesFromSharedPreferences(clothesImages: List<String>) {
+        iMainView.onSuccessImageDrawable(clothesImages)
+    }
+
+    fun rePresentData() {
+        ModifySharedPrefs().saveData(temperature)
+        ModifySharedPrefs().getDataFromSharedPrefs { clothesImages ->
             iMainView.onSuccessImageDrawable(clothesImages)
         }
     }
